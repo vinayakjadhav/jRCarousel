a8685fbc3ac4954a8409b8ff587933dbffaa2590
vinayakjadhav <vinayakrpjadhav@gmail.com>
2015-10-12 03:30:49 +0530

    initial commit

---
+++ src/jRCarousel.js
@@ -0,0 +1,19 @@
+/**
+ * Author: Vinayak Rangnathrao Jadhav
+ * Version: 0.1
+ */
+(function($){
+	
+	$.fn.jRCarousel = function(options){
+		var defaults = {
+				
+		}
+		var settings = $.extend( true, {}, defaults, options );
+		
+		
+		return this;
+	}
+	
+	
+	
+})(jQuery);
